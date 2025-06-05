e12dfbd538c26fa41046b61fb51aeedee88b3e28
Adrylen <adrien.martinez@isen-lille.fr>
2025-06-05 16:25:16 +0200

    init: base website for ipeps

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <NuxtPage />
+</template>
+
+<style>
+@import '/assets/stylesheets/global.css';
+
+body {
+  margin: 0;
+}
+
+p {
+  margin: 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  ipeps
+</template>
