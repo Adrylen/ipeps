c148e8fa60658039690b23730b04885dfd928ef1
Adrylen <adrien.martinez@isen-lille.fr>
2025-06-16 01:08:32 +0200

    fix: ensure contact card works on mobile and small screen devices

--- components/ContactCard.vue
+++ components/ContactCard.vue
@@ -3,7 +3,17 @@ import { EnvelopeIcon, DevicePhoneMobileIcon } from '@heroicons/vue/24/outline';
 </script>
 
 <template>
-  <div class="card card-border border-primary border-2 rounded-2xl bg-neutral shadow-2xl/50 px-8">
+  <div :class="[
+    'card',
+    'card-border',
+    'bg-neutral',
+    'border-primary',
+    'border-2',
+    'rounded-2xl',
+    'shadow-2xl/50',
+    'px-0', 'sm:px-8',
+    'w-full',
+  ]">
     <figure class="h-[150px]">
       <img
         alt="Logo iPeps"
@@ -18,22 +28,44 @@ import { EnvelopeIcon, DevicePhoneMobileIcon } from '@heroicons/vue/24/outline';
       <h2 class="text-2xl font-logo-secondary text-center">
         Engagée pour votre Entreprise...
       </h2>
-      <p class="text-xl p-4">
+      <p :class="[
+        'px-8', 'sm:px-4',
+        'py-4',
+        'text-center',
+        'text-xl',
+      ]">
         Conseil pour les affaires et autres conseils de gestion
       </p>
-      <div class="flex justify-between">
-        <div class="flex flex-col justify-between">
+      <div :class="[
+        'flex',
+        'flex-col', 'md:flex-row',
+        'gap-8',
+        'justify-between',
+        'py-4',
+      ]">
+        <div :class="[
+          'flex',
+          'flex-col',
+          'gap-2',
+          'items-center', 'md:items-start',
+          'md:justify-between',
+        ]">
           <span class="font-primary text-primary-content text-2xl">
             Isabelle Martinez
           </span>
           <a class="flex gap-2 text-secondary-content font-bold" href="tel:0613628421">
             <abbr title="Téléphone"><DevicePhoneMobileIcon class="size-6" /></abbr>+33 6 13 62 84 21
           </a>
-          <a class="flex gap-2 text-secondary-content font-bold" href="mailto:pepsconseils@gmail.com">
-            <abbr title="Email"><EnvelopeIcon class="size-6" /></abbr>pepsconseils@gmail.com
+          <a class="flex gap-2 text-secondary-content font-bold" href="mailto:pepsconseils@yahoo.com">
+            <abbr title="Email"><EnvelopeIcon class="size-6" /></abbr>pepsconseils@yahoo.com
           </a>
         </div>
-        <div class="flex flex-col items-end">
+        <div :class="[
+          'flex',
+          'flex-col',
+          'gap-2', 'md:gap-0',
+          'items-start', 'md:items-end',
+        ]">
           <p>
             SIRET : 98242825200018
           </p>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="bg-color-gradient h-full w-full flex flex-col items-center p-40">
-    <InformationCard />
+  <div :class="[
+    'bg-color-gradient',
+    'flex',
+    'flex-col',
+    'h-full',
+    'items-center',
+    'p-4', 'sm:p-8', 'md:p-16', 'lg:p-40',
+    'w-full',
+  ]">
+    <ContactCard />
   </div>
 </template>
